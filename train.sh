0527b063b4d92b003af3b9bf2daf8daaf4863606
idanazuri <idan.azuri@gmail.com>
2019-11-14 18:11:30 +0200

    minor

--- train.sh
+++ train.sh
@@ -13,4 +13,4 @@ nvidia-smi
 module load cuda/10.0
 module load tensorflow
 
-python3 train.py --exec_cap 50 --num_init_dags 1 --num_stream_dags 200 --reset_prob 5e-7 --reset_prob_min 5e-8 --reset_prob_decay 4e-10 --diff_reward_enabled 1 --num_agents 16 --model_save_interval 100 --model_folder ./models/stream_200_job_diff_reward_reset_5e-7_5e-8/
+python3 train.py --exec_cap 50 --num_init_dags 20 --num_stream_dags 0 --reset_prob 5e-7 --reset_prob_min 5e-8 --reset_prob_decay 4e-10 --diff_reward_enabled 1 --num_agents 16 --model_save_interval 100 --model_folder ./models/stream_200_job_diff_reward_reset_5e-7_5e-8/
