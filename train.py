0635e5912bad37b0e28d717f3583f7d0da64a4f9
idanazuri <idan.azuri@gmail.com>
2019-11-15 16:24:35 +0200

    minor

--- train.py
+++ train.py
@@ -1,6 +1,6 @@
 import os
-os.environ['CUDA_VISIBLE_DEVICES']='3'
-os.environ['TF_CPP_MIN_LOG_LEVEL']='0'
+os.environ['CUDA_VISIBLE_DEVICES']='0'
+os.environ['TF_CPP_MIN_LOG_LEVEL']='3'
 import time
 import numpy as np
 import tensorflow as tf
